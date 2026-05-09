3e6ab0711a90c20b05199ebb255533a059c8ce5c
Aman8683 <programmer.8081@gmail.com>
2026-05-09 12:49:06 +0530

    start of recursion advance questions

--- recursion/advance-recursion/index.cpp
+++ recursion/advance-recursion/index.cpp
@@ -3,7 +3,17 @@
 #include <string>
 
 using namespace std;
+// recursion multiple calls 
+
+
+// fabannacci numbers
+int f(int n) {
+    if (n <= 1) return n;
+    int last = f(n-1);
+    int secLast = f(n-2);
+    return last + secLast;
+}
 
 int main() {
-    cout << "works !";
+    cout << f(6);
 }
